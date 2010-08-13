e8d9238e8de890e76f93c1bc85ae10d53aa09e54
Chris Harrison <chris@cdharrison.com>
2010-08-13 01:07:17 -0400

    Renamed subpackage. Template converted to HTML5.

--- index.php
+++ index.php
@@ -1,60 +1,71 @@
 <?php
 /**
  * @package WordPress
- * @subpackage effercio
+ * @subpackage effercio2
  */
 
-	get_header(); 
+get_header(); 
 
 ?>
 
-   <div id="content" class="home">
-   	<h3>Latest<em>Stuff</em></h3>
-   
-	<?php if (have_posts()) : ?>
-		<?php while (have_posts()) : the_post(); ?>
-	
-			<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-				<h2 class="title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to '<?php the_title(); ?>'"><?php the_title(); ?></a></h2>
-				<p class="metadata">
-					<span class="date"><?php the_time('m.d.Y') ?></span> at
-					<span class="time"><?php the_time() ?></span> by
-					<span class="user"><?php the_author() ?></span>
-				</p>
-			
-				<div class="entry">
-					<?php the_content("<p class=\"continue\">" . __('Read the Rest of','') . " '" . the_title('', '', false) . "'</p>"); ?>
-				</div>
+		   <section id="content" class="index">
+				<?php if (have_posts()) : ?>
+					<?php while (have_posts()) : the_post(); ?>
 				
-				<div class="tools">
-					<span class="comments"><?php comments_popup_link(__('Respond'), __('Comments (1)'), __('Comments (%)'), 'commentslink', __('Comments off')); ?></span>
-					<span class="permalink"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to '<?php the_title(); ?>'">Permalink</a></span>
-					<?php edit_post_link(__('Edit'), '<span class="edit_post_link"> ', '</span>'); ?>
-				</div>
-			</div>
+						<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+							
+							<?php if ( has_post_thumbnail() ) : ?>
+								<?php the_post_thumbnail( 'medium' ) ?>
+							<?php endif; ?>
+							
+							<time datetime="<?php the_time( 'Y-m-d' ) ?>T<?php the_time() ?>-04:00">
+								<span class="month"><?php the_time( 'm' ) ?></span>
+								<span class="day"><?php the_time( 'd' ) ?></span>
+								<span class="year"><?php the_time( 'y' ) ?></span>
+							</time>
+							
+							<header class="title">
+								<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to '<?php the_title(); ?>'"><?php the_title(); ?></a></h2>
+							</header>
+										
+							<section class="entry">
+								<?php the_content( 'Continue Reading...' ); ?>
+							</section>
+							
+							<section class="post-meta">
+								<span class="permalink">
+									<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to '<?php the_title(); ?>'">#</a>
+								</span>
+								<span class="comments">
+									<?php comments_popup_link(__('Leave a Comment'), __('1 Comment'), __('% Comments'), 'commentslink', __('Comments Off')); ?>
+								</span>
+								<?php edit_post_link(__('Edit'), '<span class="edit-post-link"> ', '</span>'); ?>
+							</section>
+								
+						</article>
+						
+							
+					<? endwhile; ?>
+							
+					<section class="browse clearfix">
 					
-		<? endwhile; ?>
+						<?php posts_nav_link(' ','&#8592; Previous Entries','Next Entries &#8594;'); ?>
 					
-			<div class="browse bottom clearfix">
-				<?php posts_nav_link(' &#151; ','Previous Entries','Next Entries'); ?> 
-			</div>
+					</section> <!-- /.browse -->
+					
+				<?php else : ?>
+				
+					<section class="error">		
+					
+						<h2>Not Found</h2>
+						<p>Sorry, but you are looking for something that isn't here.</p>
+						<?php include (TEMPLATEPATH . "/searchform.php"); ?>			
+					
+					</section> <!-- /.error -->
+						
+				<?php endif; ?>
 		
-	<?php else : ?>
-	
-		<div <?php post_class() ?>
-	
-			<h2>Not Found</h2>
-			<p>Sorry, but you are looking for something that isn't here.</p>
-			<?php include (TEMPLATEPATH . "/searchform.php"); ?>
-			
-		</div>
-			
-	<?php endif; ?>
-
-
-	
-	
-	</div>
+			</section> <!-- /#content -->
 
 <?php
 	get_sidebar();
