5f5a8add23ce6befdce49192e0d26f64c958d031
Chris Harrison <chris@cdharrison.com>
2010-08-13 01:06:38 -0400

    Added support for Menus.

--- functions.php
+++ functions.php
@@ -1,6 +1,17 @@
 <?php
 
 	// effercio2 by cdharrison
+	
+	// Menus
+	add_action( 'init', 'register_my_menus' );
+    function register_my_menus() {
+        register_nav_menus(
+            array(
+                'menu-1' => __( 'Menu 1' ),
+                'menu-2' => __( 'Menu 2' )
+            )
+        );
+    }
 
 	// Sidebars
 		if ( function_exists( 'register_sidebars' ) )
