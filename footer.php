e79c6d51bb7b275d4c2949045a726b27a4445c23
Chris Harrison <chris@cdharrison.com>
2010-08-13 01:17:58 -0400

    HTML5ified. Added dynamic footer nav. Like a boss.

--- footer.php
+++ footer.php
@@ -5,32 +5,32 @@
  */
 ?>
 
-				<div class="clearfix"></div>
-			</div>
-
-			<!-- start footer -->
-			<div id="footer">
+			<footer>
+			
+			    <nav>
+				    <?php wp_nav_menu( array('menu' => 'Footer Nav' )); ?>
+			    </nav>
+			
 				<ul id="credits">
 					<!-- Support Wordpress by keeping this link intact. -->
 					<li class="wp">Proudly powered by <a href="http://wordpress.org" title="WordPress">WordPress</a>.</li>
 					<!-- Easiest way to say thanks for this theme is to keep the link to my site.
 						 If you feel compelled to remove it, though, you won't hurt my feelings. -->
 					<li class="theme-author">
-						<strong>Effercio 1.5&alpha;</strong> theme by
+						<strong>Effercio 2&alpha;</strong> theme by
 						<a href="http://cdharrison.com" title="Chris Harrison">Chris Harrison</a>
 					</li>
 					<li class="entries rss"><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a></li>
 					<li class="comments rss"><a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a></li>
 				</ul>
 				
-				<!-- Design by Chris Harrison - http://cdharrison.com/themes/effercio/ -->
 				<?php wp_footer(); ?>
-				<!-- <?php echo get_num_queries(); ?> -->
-			</div>
+				
+				<?php echo get_num_queries(); ?>
+				
+			</footer>
 			
-			<div style="clear:both;"></div>
-		</div>
-		<!-- /end footer -->
+		</section> <!-- /#container -->
         
 	</body>
 </html>
