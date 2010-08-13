9023629d149b289727ad5e16cda875e960d369bb
Chris Harrison <chris@cdharrison.com>
2010-08-13 01:32:35 -0400

    Introduced some of the code present in TwentyTen to simplify things in the header.

--- header.php
+++ header.php
@@ -3,17 +3,12 @@
  * @package WordPress
  * @subpackage effercio2
  */
-?>
-<!DOCTYPE html>
-<html>
-	<head profile="http://gmpg.org/xfn/11">
-		<meta charset="<?php bloginfo('charset'); ?>" />
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		
-		<title><?php wp_title( '&laquo;', true, 'right' ); ?> <?php bloginfo( 'name' ); ?></title>
-
-		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo( 'name' ); ?> Posts" href="<?php bloginfo( 'rss2_url' ); ?>" />
-		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo( 'name' ); ?> Comments" href="<?php bloginfo( 'comments_rss2_url' ); ?>" />
-		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+		<title><?php wp_title( '|', true, 'right' ); ?> <?php bloginfo( 'name' ); ?></title>
 
 		<meta name="description" content="" /> <?php // This will be something that can be changed via Options in near future. ?>
 		<meta name="generator" content="WordPress <?php bloginfo( 'version' ); ?>" />
@@ -27,13 +22,27 @@
             <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
 		
-        <style type="text/css" media="screen">
-			@import url('<?php bloginfo( 'stylesheet_url '); ?>');
-		</style>
-
-		<?php wp_get_archives( 'type=monthly&format=link' ); ?>
-		<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
-		<?php wp_head(); ?>        
+		<link rel="profile" href="http://gmpg.org/xfn/11" />
+        <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo( 'name' ); ?> Posts" href="<?php bloginfo( 'rss2_url' ); ?>" />
+		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo( 'name' ); ?> Comments" href="<?php bloginfo( 'comments_rss2_url' ); ?>" />
+		
+        <?php
+            /* We add some JavaScript to pages with the comment form
+             * to support sites with threaded comments (when in use).
+             */
+            if ( is_singular() && get_option( 'thread_comments' ) )
+                wp_enqueue_script( 'comment-reply' );
+        
+            /* Always have wp_head() just before the closing </head>
+             * tag of your theme, or you will break many plugins, which
+             * generally use this hook to add elements to <head> such
+             * as styles, scripts, and meta tags.
+             */
+            wp_head();
+        ?>
+      
 	</head>
 
 	<body>
